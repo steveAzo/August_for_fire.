4d1cec5310f5d1c0a10e1597b40db8a0df6392f6
steveAzo <135361078+steveAzo@users.noreply.github.com>
2023-08-23 14:08:00 +0000

    try and error

---
+++ try_error.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple PHP Calculator</title>
+</head>
+<body>
+    <h2>Simple PHP Calculator</h2>
+    <form method="post">
+        <input type="number" name="num1" placeholder="Enter number 1" required>
+        <select name="operator">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select>
+        <input type="number" name="num2" placeholder="Enter number 2" required>
+        <input type="submit" value="Calculate">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $num1 = $_POST["num1"];
+        $num2 = $_POST["num2"];
+        $operator = $_POST["operator"];
+
+        switch ($operator) {
+            case "+":
+                $result = $num1 + $num2;
+                break;
+            case "-":
+                $result = $num1 - $num2;
+                break;
+            case "*":
+                $result = $num1 * $num2;
+                break;
+            case "/":
+                if ($num2 != 0) {
+                    $result = $num1 / $num2;
+                } else {
+                    $result = "Cannot divide by zero";
+                }
+                break;
+            default:
+                $result = "Invalid operator";
+        }
+
+        echo "<h3>Result: $result</h3>";
+    }
+    ?>
+</body>
+</html>
